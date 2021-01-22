5eeda934f271a3c24ed6f360b5f563981e9e5bbe
Sebastian Herrera <SHerrera@eatonvance.com>
2021-01-22 12:27:15 -0300

    loads appsettings and consume

--- RetrivePatternFromFile/Program.cs
+++ RetrivePatternFromFile/Program.cs
@@ -1,6 +1,7 @@
 using System.IO;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace RetrivePatternFromFile
@@ -12,6 +13,10 @@ namespace RetrivePatternFromFile
 
 
             LoadAppsettingValuesFromSomewhere();
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json")
+                .Build();
+
+            string keyFromappsettings = configuration["MyKey"];
 
             CreateHostBuilder(args).Build().Run();
         }
